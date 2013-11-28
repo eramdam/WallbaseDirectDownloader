4b55486e7379c9a2cdd37cdc42555a94727e9eb0
Damien Erambert <damien.erambert@me.com>
2013-11-28 15:16:57 +0100

    Moved addWallLink() outside of document.ready so it will be defined outside (useful for addEventListener('DOMNodeInserted'))

--- source/content.js
+++ source/content.js
@@ -1,13 +1,13 @@
+function addWallLink($el) {
+	var tLink = $el.find("img").attr('data-original');
+	var wallLink = tLink.replace("thumbs","wallpapers");
+	var wallLink = wallLink.replace("thumb-","wallpaper-");
+	$el.append('<div class="dl"><a href='+wallLink+'>Download</a></div>');
+}
+
 $(document).ready(function() {
 	$('section#thumbs div.thumbnail > div.wrapper').each(function(index, el) {
 		addWallLink($(el));
 	});
-
-	function addWallLink($el) {
-		var tLink = $el.find("img").attr('data-original');
-		var wallLink = tLink.replace("thumbs","wallpapers");
-		var wallLink = wallLink.replace("thumb-","wallpaper-");
-		$el.append('<div class="dl"><a href='+wallLink+'>Download</a></div>');
-	}
 });
 
