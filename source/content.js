151575792c7f25590d9f1b0ec450061228345982
Damien Erambert <damien.erambert@me.com>
2013-11-28 12:42:08 +0100

    Made a function for better code later

--- source/content.js
+++ source/content.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 	$('section#thumbs div.thumbnail > div.wrapper').each(function(index, el) {
-		var thumbLink = $("img",this).attr('data-original');
-		var wallLink = thumbLink.replace("thumbs","wallpapers");
-		var wallLink = wallLink.replace("thumb-","wallpaper-");
-		$(this).append('<div class="dl"><a href='+wallLink+' >Download</a></div>');
+		addWallLink(el);
 	});
+
+	function addWallLink($el) {
+		var tLink = $el.attr('data-original');
+		var wallLink = tLink.replace("thumbs","wallpapers");
+		var wallLink = wallLink.replace("thumb-","wallpaper-");
+		$(this).append('<div class="dl"><a href='+wallLink+'>Download</a></div>');)
+	}
 });
 
