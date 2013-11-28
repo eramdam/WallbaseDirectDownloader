99a4c8b7b09b9d03878b871ffea3300bd234539b
Damien Erambert <damien.erambert@me.com>
2013-11-28 14:20:18 +0100

    Fixed the addWallLink function and its call

--- source/content.js
+++ source/content.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	$('section#thumbs div.thumbnail > div.wrapper').each(function(index, el) {
-		addWallLink(el);
+		addWallLink($(el));
 	});
 
 	function addWallLink($el) {
-		var tLink = $el.attr('data-original');
+		var tLink = $el.find("img").attr('data-original');
 		var wallLink = tLink.replace("thumbs","wallpapers");
 		var wallLink = wallLink.replace("thumb-","wallpaper-");
-		$(this).append('<div class="dl"><a href='+wallLink+'>Download</a></div>');)
+		$el.append('<div class="dl"><a href='+wallLink+'>Download</a></div>');
 	}
 });
 
