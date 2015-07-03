342c661c5946e62f25af34f73b0cc63318b1e4d9
DraperStudio <hello@draperstud.io>
2015-07-03 11:26:23 +0200

    Make code more readable

--- source/content.js
+++ source/content.js
@@ -1,13 +1,13 @@
-
 (function wallbaseDownloadFromSearch() {
     var updating = false;
 
-    function urlCheck(url, callback){
+    function urlCheck(url, callback) {
         var request = new XMLHttpRequest;
         request.open('HEAD', url, true);
         request.send();
-        request.onreadystatechange = function(){
-            if(request.readyState==4){
+
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
                 callback(request)
             }
         }
@@ -15,37 +15,41 @@
 
     // Function to add a download link to a thumbnail element
     function addDownloadLink(element) {
-        if(!element.getElementsByClassName('wbs_dl')[0]) {
+        if (!element.getElementsByClassName('wbs_dl')[0]) {
             var thumbnailImage,
-            thumbnailLink,
-            downloadLink,
-            downloadDiv,
-            downloadAnchor,
-            previewAnchor;
+                thumbnailLink,
+                downloadLink,
+                downloadDiv,
+                downloadAnchor,
+                previewAnchor;
 
             // Get the thumbnail URL and create a download URL from it
             thumbnailImage = element.getElementsByTagName('img')[0];
 
-            if(!element.getElementsByTagName('img')[0].getAttribute('data-src'))
+            if (!element.getElementsByTagName('img')[0].getAttribute('data-src')) {
                 thumbnailLink = thumbnailImage.getAttribute('src');
-            else
+            } else {
                 thumbnailLink = thumbnailImage.getAttribute('data-src');
+            }
 
 
-            downloadLink = thumbnailLink.replace(/\/thumb\/[a-z]+\/th-/g,'/full/wallhaven-');
+            downloadLink = thumbnailLink.replace(/\/thumb\/[a-z]+\/th-/g, '/full/wallhaven-');
 
             downloadDiv = document.createElement('div');
             downloadDiv.className = 'wbs_dl wbs_unsafe';
+
             downloadAnchor = document.createElement('a');
             downloadAnchor.setAttribute('alt', 'Download');
-            downloadAnchor.setAttribute('href',downloadLink);
-            downloadAnchor.setAttribute('download','');
+            downloadAnchor.setAttribute('href', downloadLink);
+            downloadAnchor.setAttribute('download', '');
             downloadAnchor.classList.add('icon-download');
+
             previewAnchor = document.createElement('a');
             previewAnchor.setAttribute('alt', 'Open in new tab');
-            previewAnchor.setAttribute('href',downloadLink);
-            previewAnchor.setAttribute('target','_blank');
+            previewAnchor.setAttribute('href', downloadLink);
+            previewAnchor.setAttribute('target', '_blank');
             previewAnchor.classList.add('icon-eye');
+
             downloadDiv.appendChild(previewAnchor);
             downloadDiv.appendChild(downloadAnchor);
             element.appendChild(downloadDiv);
@@ -54,27 +58,31 @@
 
     function fixLinks() {
         toFix = document.getElementsByClassName('wbs_unsafe');
+
         for (var i = 0; i < toFix.length; i++) {
             var link = toFix[i];
+
             urlCheck(link.querySelector('a').href, function(request) {
-                if(request.status===404) {
-                    linkFixed = link.querySelector('a').href.replace(/jpg$/,'png');
+                if (request.status === 404) {
+                    linkFixed = link.querySelector('a').href.replace(/jpg$/, 'png');
                     link.querySelectorAll('a[class^="icon-"]')[0].setAttribute('href', linkFixed);
                     link.querySelectorAll('a[class^="icon-"]')[1].setAttribute('href', linkFixed);
                 }
             });
-            link.classList.remove('wbs_unsafe');
 
+            link.classList.remove('wbs_unsafe');
         };
     }
 
     // Document load handler
     function addDownloadLinks() {
         var wrappers = document.querySelector('#thumbs, #tag-thumbs').querySelectorAll('li figure.thumb');
+
         for (var i = 0, l = wrappers.length; i < l; i++) {
             addDownloadLink(wrappers.item(i));
             fixLinks();
         }
+
         updating = false;
     };
 
@@ -95,7 +103,7 @@
 
         var downloadLink = document.createElement('a');
         downloadLink.setAttribute('href', wallpaperLink);
-        downloadLink.setAttribute('download','');
+        downloadLink.setAttribute('download', '');
         downloadLink.insertAdjacentHTML('afterbegin', '<i class="icon icon-download"></i> Download this wallpaper');
         downloadButton.appendChild(downloadLink);
 
@@ -110,6 +118,4 @@
     } else if (document.getElementById('showcase-sidebar')) {
         addDownloadLinksOnWallpapersPage();
     }
-
 }());
-
