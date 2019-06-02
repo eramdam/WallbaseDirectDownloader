64b8ee27effbaad4ebda1747ad65aa97b42b7375
Damien Erambert <damien.erambert@me.com>
2019-06-02 09:42:49 -0700

    :memo: add comments

--- source/content.js
+++ source/content.js
@@ -6,54 +6,63 @@ function urlCheck(url) {
 }
 
 async function maybeGetFixedUrl(url) {
+  // Perform a HEAD request.
   const res = await urlCheck(url);
+  // If no error, return the OG url.
   if (res.status === 200) {
     return url;
   }
 
+  // If not, return the PNG one.
   return url.replace(/jpg$/, 'png');
 }
 
 function addDownloadLinksToThumbnails() {
+  // Grab our thumbnails.
   const wrappers = document
     .querySelector('#thumbs, #tag-thumbs')
     .querySelectorAll('li figure.thumb');
 
+  // Add links to every one of them.
   Array.from(wrappers).forEach(item => {
     addDownloadLinksToThumbnail(item);
   });
 }
 
 async function addDownloadLinksToThumbnail(element) {
+  // If we already added downloads links, nothing to do.
   if (element.querySelector('.wbs_dl')) {
     return;
   }
 
-  // Get the thumbnail URL and create a download URL from it
+  // Find the image element.
   const thumbnailImage = element.querySelector('img');
 
+  // If there is no image element, nothing to do.
   if (!thumbnailImage) {
     return;
   }
 
-  let thumbnailLink = '';
+  // Grab the url of the thumbnail.
+  const thumbnailLink =
+    thumbnailImage.getAttribute('data-src') || thumbnailImage.getAttribute('src');
 
-  if (!thumbnailImage.getAttribute('data-src')) thumbnailLink = thumbnailImage.getAttribute('src');
-  else thumbnailLink = thumbnailImage.getAttribute('data-src');
-
+  // Change it so we obtain the final wallpaper URL.
   const downloadLink = thumbnailLink
     .replace(/\/small\/([0-9a-z]+)\//i, '/full/$1/wallhaven-')
     .replace('th.wallhaven', 'w.wallhaven');
 
+  // We do a request to see if we fix the URL or not.
   const fixedDownloadLink = await maybeGetFixedUrl(downloadLink);
 
+  // Create our download and preview links.
   const downloadDiv = document.createElement('div');
   downloadDiv.className = 'wbs_dl wbs_unsafe';
+
   const downloadAnchor = document.createElement('a');
   downloadAnchor.setAttribute('alt', 'Download');
   downloadAnchor.setAttribute('target', '_blank');
   downloadAnchor.setAttribute('href', fixedDownloadLink);
-  downloadAnchor.setAttribute('download', '');
   downloadAnchor.classList.add('icon-download');
 
   const previewAnchor = document.createElement('a');
@@ -62,18 +71,25 @@ async function addDownloadLinksToThumbnail(element) {
   previewAnchor.setAttribute('target', '_blank');
   previewAnchor.classList.add('icon-eye');
 
+  // Add a handler to the download element.
   downloadAnchor.onclick = e => {
     e.preventDefault();
+    // We used to rely on the `download` attribute but browsers now restrict it to same-origin URLs.
+    // Since Wallhaven now hosts files on a different domain than the main site, we need to use file-saver to trigger a download.
     saveAs(e.target.href, new URL(e.target.href).pathname.split('/').reverse()[0]);
   };
 
+  // Add our links to the DOM.
   downloadDiv.appendChild(previewAnchor);
   downloadDiv.appendChild(downloadAnchor);
   element.appendChild(downloadDiv);
 }
 
 function addDownloadLinkOnWallpaperPage() {
+  // Get the link to the wallpaper file.
   const wallpaperLink = document.querySelector('#wallpaper').getAttribute('src');
+
+  // Create our button/link.
   const downloadButton = document.createElement('div');
   downloadButton.className = 'button';
   downloadButton.id = 'wbs-dl-button';
@@ -88,17 +104,23 @@ function addDownloadLinkOnWallpaperPage() {
   downloadLink.insertAdjacentElement('afterbegin', iconElement);
   downloadButton.appendChild(downloadLink);
 
+  // Add a handler to the download element.
   downloadButton.onclick = e => {
     e.preventDefault();
+    // We used to rely on the `download` attribute but browsers now restrict it to same-origin URLs.
+    // Since Wallhaven now hosts files on a different domain than the main site, we need to use file-saver to trigger a download.
     saveAs(wallpaperLink, new URL(wallpaperLink).pathname.split('/').reverse()[0]);
   };
 
+  // Add the button to the DOM.
   document
     .querySelector('.sidebar-section[data-storage-id="showcase-tools"] .showcase-tools')
     .insertAdjacentElement('beforebegin', downloadButton);
 }
 
+// Hacky way to wait for everything to be ready
 setTimeout(() => {
+  // If we're on a tag/list page
   if (document.querySelector('#thumbs, #tag-thumbs')) {
     const observer = new MutationObserver(() => {
       Array.from(document.querySelectorAll('figure.thumb')).forEach(el => {
@@ -106,6 +128,7 @@ setTimeout(() => {
       });
     });
 
+    // Set-up an observer that'll run everytime new elements are added to the page.
     observer.observe(document.querySelector('#thumbs, #tag-thumbs'), {
       childList: true
     });
