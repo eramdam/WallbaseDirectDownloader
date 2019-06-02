1c7409b3f1f2fe2696629e4032a5389661d9c7c1
Damien Erambert <damien.erambert@me.com>
2019-06-02 09:36:28 -0700

    :bulb: refactor/rewrite/whatever

--- source/content.js
+++ source/content.js
@@ -1,141 +1,117 @@
 import {saveAs} from 'file-saver';
+function urlCheck(url) {
+  return fetch(url, {
+    method: 'HEAD'
+  });
+}
 
-(function wallbaseDownloadFromSearch() {
-  var updating = false;
-
-  function urlCheck(url, callback) {
-    var request = new XMLHttpRequest();
-    request.open('HEAD', url, true);
-    request.send();
-    request.onerror = console.log;
-    request.onreadystatechange = function() {
-      if (request.readyState == 4) {
-        callback(request);
-      }
-    };
+async function maybeGetFixedUrl(url) {
+  const res = await urlCheck(url);
+  if (res.status === 200) {
+    return url;
   }
 
-  // Function to add a download link to a thumbnail element
-  function addDownloadLink(element) {
-    if (!element.getElementsByClassName('wbs_dl')[0]) {
-      var thumbnailImage, thumbnailLink, downloadLink, downloadDiv, downloadAnchor, previewAnchor;
+  return url.replace(/jpg$/, 'png');
+}
 
-      // Get the thumbnail URL and create a download URL from it
-      thumbnailImage = element.getElementsByTagName('img')[0];
+function addDownloadLinksToThumbnails() {
+  const wrappers = document
+    .querySelector('#thumbs, #tag-thumbs')
+    .querySelectorAll('li figure.thumb');
 
-      if (!element.getElementsByTagName('img')[0].getAttribute('data-src'))
-        thumbnailLink = thumbnailImage.getAttribute('src');
-      else thumbnailLink = thumbnailImage.getAttribute('data-src');
+  Array.from(wrappers).forEach(item => {
+    addDownloadLinksToThumbnail(item);
+  });
+}
 
-      downloadLink = thumbnailLink
-        .replace(/\/small\/([0-9a-z]+)\//i, '/full/$1/wallhaven-')
-        .replace('th.wallhaven', 'w.wallhaven');
-
-      downloadDiv = document.createElement('div');
-      downloadDiv.className = 'wbs_dl wbs_unsafe';
-      downloadAnchor = document.createElement('a');
-      downloadAnchor.setAttribute('alt', 'Download');
-      downloadAnchor.setAttribute('target', '_blank');
-      downloadAnchor.setAttribute('href', downloadLink);
-      downloadAnchor.setAttribute('download', '');
-      downloadAnchor.classList.add('icon-download');
-
-      previewAnchor = document.createElement('a');
-      previewAnchor.setAttribute('alt', 'Open in new tab');
-      previewAnchor.setAttribute('href', downloadLink);
-      previewAnchor.setAttribute('target', '_blank');
-      previewAnchor.classList.add('icon-eye');
-
-      downloadAnchor.onclick = e => {
-        e.preventDefault();
-        saveAs(downloadLink, new URL(downloadLink).pathname.split('/').reverse()[0]);
-      };
-
-      downloadDiv.appendChild(previewAnchor);
-      downloadDiv.appendChild(downloadAnchor);
-      element.appendChild(downloadDiv);
-    }
+async function addDownloadLinksToThumbnail(element) {
+  if (element.querySelector('.wbs_dl')) {
+    return;
   }
 
-  function fixLinks() {
-    var toFix = document.getElementsByClassName('wbs_unsafe');
-    if (!toFix) {
-      console.log('no unsafe');
-      return;
-    }
+  // Get the thumbnail URL and create a download URL from it
+  const thumbnailImage = element.querySelector('img');
 
-    for (var i = 0; i < toFix.length; i++) {
-      var link = toFix[i];
-      urlCheck(link.querySelector('a').href, function(request) {
-        console.log({request, status: request.status});
-        if (request.status === 404) {
-          linkFixed = link.querySelector('a').href.replace(/jpg$/, 'png');
-          link.querySelectorAll('a[class^="icon-"]')[0].setAttribute('href', linkFixed);
-        }
-      });
-      link.classList.remove('wbs_unsafe');
-    }
+  if (!thumbnailImage) {
+    return;
   }
 
-  // Document load handler
-  function addDownloadLinks() {
-    var wrappers = document
-      .querySelector('#thumbs, #tag-thumbs')
-      .querySelectorAll('li figure.thumb');
+  let thumbnailLink = '';
 
-    Array.from(wrappers).forEach(item => {
-      try {
-        addDownloadLink(item);
-        fixLinks();
-      } catch (e) {
-        console.error(e);
-      }
-    });
-    updating = false;
-  }
+  if (!thumbnailImage.getAttribute('data-src')) thumbnailLink = thumbnailImage.getAttribute('src');
+  else thumbnailLink = thumbnailImage.getAttribute('data-src');
 
-  // Document update handler
-  function updateDownloadLinks(event) {
-    if (event.relatedNode.querySelectorAll('.thumb') && !updating) {
-      updating = true;
-      addDownloadLinks();
-    }
-  }
+  const downloadLink = thumbnailLink
+    .replace(/\/small\/([0-9a-z]+)\//i, '/full/$1/wallhaven-')
+    .replace('th.wallhaven', 'w.wallhaven');
 
-  function addDownloadLinksOnWallpapersPage() {
-    var wallpaperLink = document.getElementById('wallpaper').getAttribute('src');
-    var downloadButton = document.createElement('div');
-    downloadButton.className = 'button';
-    downloadButton.id = 'wbs-dl-button';
+  const fixedDownloadLink = await maybeGetFixedUrl(downloadLink);
 
-    var downloadLink = document.createElement('a');
-    var iconElement = document.createElement('i');
-    iconElement.className = 'icon icon-download';
+  const downloadDiv = document.createElement('div');
+  downloadDiv.className = 'wbs_dl wbs_unsafe';
+  const downloadAnchor = document.createElement('a');
+  downloadAnchor.setAttribute('alt', 'Download');
+  downloadAnchor.setAttribute('target', '_blank');
+  downloadAnchor.setAttribute('href', fixedDownloadLink);
+  downloadAnchor.setAttribute('download', '');
+  downloadAnchor.classList.add('icon-download');
 
-    downloadLink.setAttribute('href', wallpaperLink);
-    downloadLink.setAttribute('target', '_blank');
-    downloadLink.insertAdjacentText('beforeend', ' Download this wallpaper');
-    downloadLink.insertAdjacentElement('afterbegin', iconElement);
-    downloadButton.appendChild(downloadLink);
+  const previewAnchor = document.createElement('a');
+  previewAnchor.setAttribute('alt', 'Open in new tab');
+  previewAnchor.setAttribute('href', fixedDownloadLink);
+  previewAnchor.setAttribute('target', '_blank');
+  previewAnchor.classList.add('icon-eye');
 
-    downloadButton.onclick = e => {
-      e.preventDefault();
-      saveAs(wallpaperLink, new URL(wallpaperLink).pathname.split('/').reverse()[0]);
-    };
+  downloadAnchor.onclick = e => {
+    e.preventDefault();
+    saveAs(e.target.href, new URL(e.target.href).pathname.split('/').reverse()[0]);
+  };
 
-    document
-      .querySelector('.sidebar-section[data-storage-id="showcase-tools"] .showcase-tools')
-      .insertAdjacentElement('beforebegin', downloadButton);
-  }
+  downloadDiv.appendChild(previewAnchor);
+  downloadDiv.appendChild(downloadAnchor);
+  element.appendChild(downloadDiv);
+}
 
-  // The thumbnail list will be updated on scroll
+function addDownloadLinkOnWallpaperPage() {
+  const wallpaperLink = document.querySelector('#wallpaper').getAttribute('src');
+  const downloadButton = document.createElement('div');
+  downloadButton.className = 'button';
+  downloadButton.id = 'wbs-dl-button';
+
+  const downloadLink = document.createElement('a');
+  const iconElement = document.createElement('i');
+  iconElement.className = 'icon icon-download';
+
+  downloadLink.setAttribute('href', wallpaperLink);
+  downloadLink.setAttribute('target', '_blank');
+  downloadLink.insertAdjacentText('beforeend', ' Download this wallpaper');
+  downloadLink.insertAdjacentElement('afterbegin', iconElement);
+  downloadButton.appendChild(downloadLink);
+
+  downloadButton.onclick = e => {
+    e.preventDefault();
+    saveAs(wallpaperLink, new URL(wallpaperLink).pathname.split('/').reverse()[0]);
+  };
+
+  document
+    .querySelector('.sidebar-section[data-storage-id="showcase-tools"] .showcase-tools')
+    .insertAdjacentElement('beforebegin', downloadButton);
+}
+
+setTimeout(() => {
   if (document.querySelector('#thumbs, #tag-thumbs')) {
-    document
-      .querySelector('#thumbs, #tag-thumbs')
-      .addEventListener('DOMNodeInserted', updateDownloadLinks);
-    addDownloadLinks();
-    fixLinks();
-  } else if (document.getElementById('showcase-sidebar')) {
-    addDownloadLinksOnWallpapersPage();
+    const observer = new MutationObserver(() => {
+      Array.from(document.querySelectorAll('figure.thumb')).forEach(el => {
+        addDownloadLinksToThumbnail(el);
+      });
+    });
+
+    observer.observe(document.querySelector('#thumbs, #tag-thumbs'), {
+      childList: true
+    });
+
+    addDownloadLinksToThumbnails();
+  } else if (document.querySelector('#showcase-sidebar')) {
+    addDownloadLinkOnWallpaperPage();
   }
-})();
+}, 0);
