3a228d8758629e82191484d3851d6742c29d0e39
Damien Erambert <damien.erambert@me.com>
2021-02-28 20:43:44 -0800

    Add Safari tooling

---
+++ safari/Wallhaven Direct Downloader/Wallhaven Direct Downloader/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Wallhaven Direct Downloader
+//
+//  Created by Damien Erambert on 2/28/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
